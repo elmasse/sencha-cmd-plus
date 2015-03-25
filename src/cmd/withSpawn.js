f9c81c0a1517c7c6b4bb53068fa61ef7e067b938
Maximiliano Fierro <elmasse@gmail.com>
2015-03-25 17:36:52 -0300

    output cmd process on stdout. Preserved colored output.

--- src/cmd/withSpawn.js
+++ src/cmd/withSpawn.js
@@ -7,29 +7,8 @@ cocktail.mix({
     '@exports': module,
     '@as': 'class',
 
-    '@requires': [
-        'getArgs'
-    ],
-
     runSpawn: function (which, args) {
-        this._attachSpawnListeners(spawn(which, args));
-    },
-
-    _attachSpawnListeners: function (spawnInstance) {
-        var me = this;
-        me._attachEventListener('data', spawnInstance.stdout, me._onProgress);
-        me._attachEventListener('error', spawnInstance.stderr, me._onError);
-    },
-
-   _attachEventListener: function (event, stream, mthd) {
-        stream.on(event, mthd.bind(this));
-    },
-
-    _onProgress: function (data) {
-        console.log('' + data);
-    },
-
-    _onError: function (data) {
-        console.error('Error: ' + data);
+        spawn(which, args, {stdio: "inherit"});
     }
+
 });
